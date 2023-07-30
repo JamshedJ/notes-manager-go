075607be7f92a78b707a5f9f29c5cf13e4dc38e7
Jamshed <jamshedjuraev95@gmail.com>
2023-07-30 21:32:31 +0500

    added connection to database

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,11 @@ import (
 	"github.com/JamshedJ/todo/pkg/handler"
 	"github.com/JamshedJ/todo/pkg/repository"
 	"github.com/JamshedJ/todo/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -15,7 +18,23 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repo := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading .env valiables: %s", err.Error())
+	}
+
+	db, err := repository.ConnectionDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed initialize db: %s", err.Error())
+	}
+
+	repo := repository.NewRepository(db)
 	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 
