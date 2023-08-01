43c3e6fdb20fa23da8c2651191cbe36d9a631139
Jamshed <jamshedjuraev95@gmail.com>
2023-08-01 11:31:02 +0500

    added generateHash

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -1,8 +1,14 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"github.com/JamshedJ/todo/models"
 	"github.com/JamshedJ/todo/pkg/repository"
+)
+
+const (
+	salt = "ksdhgfi39jvbip39p0nlfnd"
 )
 
 type AuthService struct {
@@ -14,5 +20,13 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user models.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
