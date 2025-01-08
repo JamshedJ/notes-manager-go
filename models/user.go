0595a31c9f0a04c92d42234a07522ee735c2aaab
Jamshed <juraev@bankplanet9.com>
2025-01-08 14:37:09 +0500

    init commit

--- models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID       int    `json:"-" db:"id"`
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
